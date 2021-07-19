cf0575c7abaad30aeb56a47035ed0c14aef5efd2
Evan Simon <evansimon2001@gmail.com>
2021-07-18 23:30:16 -0500

    Functioning Scrapper

--- cs225_web_scrapper.py
+++ cs225_web_scrapper.py
@@ -2,12 +2,13 @@ import os
 from requests import get
 from bs4 import BeautifulSoup
 
-URL = "https://courses.engr.illinois.edu/cs225/sp2021/pages/lectures.html"
+URL = "https://courses.engr.illinois.edu/cs225/sp2021/pages/lectures.html"  # target URL
 
 if __name__ == "__main__":
     page_html = get(URL).text
     page = BeautifulSoup(page_html, features="html.parser")
     weeks = page.find_all("div", class_="row flex-column-reverse flex-md-row lecture-row mb-4 mx-2")
+    weeks.reverse()
     week_num = 0
     for week in weeks:
         week_num += 1
@@ -23,7 +24,12 @@ if __name__ == "__main__":
                 continue
             link = "https://courses.engr.illinois.edu/" + slide_block.get("href")   # adds base URL
             if "slides" in link:
-                print(link)
+                file_name = f"{folder_name}/{title} on {date}.pdf"
+                print(file_name)
+                slide_content = get(link).content
+                with open(file_name, "wb") as slide_file:
+                    slide_file.write(slide_content)
+                print("Downloaded " + file_name)
             else:   # if there are no slides, like for exam days, continue
                 continue
 
