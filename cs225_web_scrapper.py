4e2cf3fc86e29f6b0bd286c81dde629e1112ad5f
Evan Simon <evansimon2001@gmail.com>
2021-07-18 23:51:43 -0500

    100% on pylint syntax

--- cs225_web_scrapper.py
+++ cs225_web_scrapper.py
@@ -1,3 +1,5 @@
+"""Downloads slides from https://courses.engr.illinois.edu/cs225/sp2021/pages/lectures.html
+and stores them in folders by week with name and date"""
 import os
 from requests import get
 from bs4 import BeautifulSoup
@@ -9,19 +11,19 @@ if __name__ == "__main__":
     page = BeautifulSoup(page_html, features="html.parser")
     weeks = page.find_all("div", class_="row flex-column-reverse flex-md-row lecture-row mb-4 mx-2")
     weeks.reverse()
-    week_num = 0
+    WEEK_NUM = 0
     for week in weeks:
-        week_num += 1
-        folder_name = f"week{week_num}"
+        WEEK_NUM += 1
+        folder_name = f"Week {WEEK_NUM}"
         os.makedirs(folder_name, exist_ok=True)
         days = week.find_all("div", class_="card-body")
         for day in days:
             title = day.find("h5").text
-            date = day.find("div").text            
-            try:
-                slide_block = day.find("ul").find("li").find("a")   # only checks first block but slide is always the first block
-            except AttributeError as e: # break day has not slide_block, in which case, the program continues
-                continue
+            date = day.find("div").text
+            try:    # only checks first block but slide is always the first block
+                slide_block = day.find("ul").find("li").find("a")
+            except AttributeError as error:
+                continue    # The break day does not have slide_block, so the program continues
             link = "https://courses.engr.illinois.edu/" + slide_block.get("href")   # adds base URL
             if "slides" in link:
                 file_name = f"{folder_name}/{title} on {date}.pdf"
@@ -32,5 +34,3 @@ if __name__ == "__main__":
                 print("Downloaded " + file_name)
             else:   # if there are no slides, like for exam days, continue
                 continue
-
-
